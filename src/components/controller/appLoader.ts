b2da52e5c6f723730adabd6407355a3ae7146f4d
Yauheny-Bychkou <55757666+Yauheny-Bychkou@users.noreply.github.com>
2021-12-06 14:37:51 +0300

    link

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,8 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: 'bba9b6ea345e4a20b8a317709a46d8e0', // получите свой ключ https://newsapi.org/
+        super('https://nodenews.herokuapp.com/', {
+            apiKey: '800400cefcc64238a269ee560c199887', // получите свой ключ https://newsapi.org/
         });
     }
 }
